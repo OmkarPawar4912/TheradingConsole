dc779f430883e471a30fb1d62a2ecada9c84ff35
dev-Omkar <opawar88@gmail.com>
2021-12-10 17:58:45 +0530

    Done

--- TheradingConsole/Program.cs
+++ TheradingConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 
 namespace TheradingConsole
@@ -8,17 +9,35 @@ namespace TheradingConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("--------------------------- Without Threading ---------------------------");
+            FileStream ostrm;
+            StreamWriter writer;
+            TextWriter oldOut = Console.Out;
+            try
+            {
+                // Log File Create
+                ostrm = new FileStream("../../../Log-"+ DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt", FileMode.OpenOrCreate, FileAccess.Write);
+                writer = new StreamWriter(ostrm);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot open Redirect.txt for writing");
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            Console.SetOut(writer);
+
+           Console.WriteLine("--------------------------- Without Threading ---------------------------");
 
             DelOperation delOperation = new DelOperation(Operations.GetEvenNo);
             delOperation += Operations.GetPallidromNo;
             delOperation();
-            
+
             Console.WriteLine("--------------------------- Single thread starts ------------------------------");
-            
+
             Thread thread = new Thread(new ThreadStart(Operations.GetPallidromNo));   // Single thread  
             thread.Start();   // Start 
-            thread.Abort();   // Abort 
+
             Operations.GetEvenNo();  // w/o thread.  
 
             Console.WriteLine("--------------------------- Single thread end ---------------------------------");
@@ -27,12 +46,15 @@ namespace TheradingConsole
             Thread MultiThread1 = new Thread(Operations.GetEvenNo); //Creating the Thread    
             Thread MultiThread2 = new Thread(Operations.GetPallidromNo); //Creating the Thread    
             MultiThread1.Start(); //Starting the Thread    
-            MultiThread2.Start(); //Starting the Thread 
-            MultiThread1.Abort(); // Abort
-            MultiThread2.Abort();
-        
+            MultiThread2.Start();
+
             Console.WriteLine("--------------------------- Multi thread end ---------------------------------");
 
+            Console.SetOut(oldOut);
+            writer.Close();
+            ostrm.Close();      // Log File end
+            Console.WriteLine(" *************************** End Here ****************************************");
+            Console.ReadLine();
         }
     }
 }
