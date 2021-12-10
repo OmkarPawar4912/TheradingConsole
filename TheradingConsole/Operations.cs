55d25662031cc9aea1df0dd521b897d1bffcb7c4
dev-Omkar <opawar88@gmail.com>
2021-12-10 16:18:34 +0530

    Add project files.

---
+++ TheradingConsole/Operations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TheradingConsole
+{
+    class Operations
+    {
+        public static void GetEvenNo()
+        {
+            Console.WriteLine("\t\t\t GetEvenNo() Started");
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(" Even : " + i);
+                    //Thread.Sleep(1000);
+                }
+            }
+        }
+
+        public static void GetPallidromNo()
+        {
+            Console.WriteLine("\t\t\t GetPalindromeNo() Started");
+            int num, n, rev_no, r;
+
+            for (num = 1; num <= 1000; num++)
+            {
+                rev_no = 0;
+                n = num;
+                while (n != 0)
+                {
+                    r = n % 10;
+                    rev_no = rev_no * 10 + r;
+                    n = n / 10;
+                }
+                if (num == rev_no)
+                {
+                    Console.WriteLine("Palindrome : {0}", num);
+                    //Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
---
+++ TheradingConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace TheradingConsole
+{
+    delegate void DelOperation(); // Delegate Declaration
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------- Without Threading ---------------------------");
+
+            DelOperation delOperation = new DelOperation(Operations.GetEvenNo);
+            delOperation += Operations.GetPallidromNo;
+            delOperation();
+            
+            Console.WriteLine("--------------------------- Single thread starts ------------------------------");
+            
+            Thread thread = new Thread(new ThreadStart(Operations.GetPallidromNo));   // Single thread  
+            thread.Start();   // Start 
+
+            Operations.GetEvenNo();  // w/o thread.  
+
+            Console.WriteLine("--------------------------- Single thread end ---------------------------------");
+            Console.WriteLine("=========================== Multi thread end ==================================");
+
+            Thread MultiThread1 = new Thread(Operations.GetEvenNo); //Creating the Thread    
+            Thread MultiThread2 = new Thread(Operations.GetPallidromNo); //Creating the Thread    
+            MultiThread1.Start(); //Starting the Thread    
+            MultiThread2.Start(); //Starting the Thread 
+
+            Console.WriteLine("--------------------------- Multi thread end ---------------------------------");
+
+        }
+    }
+}
